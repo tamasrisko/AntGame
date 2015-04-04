0aced4de9516eae71c6ddd59cf0b89bf9cabe182
andrewwilsonjones <andrewwilsonjones@hotmail.com>
2015-04-04 14:08:27 +0100

    Merged Instruction into Ant

--- src/antgame/Ant.java
+++ src/antgame/Ant.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 public class Ant {
 
     // variables
-
     private Position p;	// position
     public int dir;
     public Colour c;	// colour
@@ -163,7 +162,7 @@ public class Ant {
         }
 
         for (int i = 0; i < 6; i++) {
-			// if there is an enemy ant at (xi, yi), numAnt++
+            // if there is an enemy ant at (xi, yi), numAnt++
             // else, do nothing
             // this required an method to search and return 
             // an ant object by its position
@@ -347,5 +346,56 @@ public class Ant {
     public enum LeftOrRight {
 
         LEFT, RIGHT
+    }
+    public enum Condition {
+
+        FRIEND, FOE,
+        FRIEND_WITH_FOOD, FOR_WITH_FOOD,
+        FOOD, ROCK,
+        M0, M1, M2, M3, M4, M5,
+        FOE_MARKER,
+        HOME, FOE_HOME
+    }
+
+    abstract class Instruction {
+
+        public class Sense extends Instruction {
+
+            SenseDirection sensedir;
+            int st1, st2;
+            Condition cond;
+        }
+
+        public class Mark extends Instruction {
+
+            int i, st;
+        }
+
+        public class PickUp extends Instruction {
+
+            int st1, st2;
+        }
+
+        public class Drop extends Instruction {
+
+            int st;
+        }
+
+        public class Turn extends Instruction {
+
+            LeftOrRight lr;
+            int st;
+        }
+
+        public class Move extends Instruction {
+
+            int st1, st2;
+        }
+
+        public class Flip extends Instruction {
+
+            int p, st1, st2;
+        }
+
     }
 }
