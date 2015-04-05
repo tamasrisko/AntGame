2b8d54fc16e008b0b013e49fc0ebff9f5ab6869d
falcontomto <falcontomto@gmail.com>
2015-04-05 18:31:48 +0100

    Merge Position class into Cell class

--- src/antgame/Cell.java
+++ src/antgame/Cell.java
@@ -221,4 +221,13 @@ public class Cell {
     private void increaseFood() {
         foodAmount++;
     }
+    
+    public class Position<X, Y> { 
+      public int x; 
+      public int y; 
+      public Position(int x, int y) { 
+        this.x = x; 
+        this.y = y; 
+      } 
+    } 
 }
